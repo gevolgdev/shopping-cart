da6163e558cd493a49a9e595ee56ec5c28d2c343
Pinelli <ricardosilvapinelli@gmail.com>
2023-04-23 20:06:16 -0300

    GlobalStyle: Fundo LightMode

--- src/style/GlobalStyle.ts
+++ src/style/GlobalStyle.ts
@@ -5,8 +5,8 @@ export const GlobalStyle = createGlobalStyle`
 
   body {
     font-family: 'Inter', sans-serif;
-    background: #101010;
+    background: #e9ecef;
     font-size: 16px;
-    color: #DDDDDD;
+    color: #101010;
   }
 `
