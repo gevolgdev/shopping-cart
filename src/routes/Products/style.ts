2cc502d29013c63ab5c6be4029484864132db08e
Pinelli <ricardosilvapinelli@gmail.com>
2023-04-23 20:08:30 -0300

    Products: Design

--- src/routes/Products/style.ts
+++ src/routes/Products/style.ts
@@ -20,14 +20,53 @@ export const ProductsContainer = styled.section`
 
 export const Title = styled.h1`
   font-size: 2.5rem;
-  color: white;
   margin-bottom: 40px;
 `
 
 export const Product = styled.div`
   display: flex;
-  width: 300px;
-  height: 300px;
-  background: #404040;
+  flex-direction: column;
+  width: 350px;
+  background: white;
   padding: 15px;
+  filter: drop-shadow(0 20px 13px rgb(0 0 0 / 0.03)) drop-shadow(0 8px 5px rgb(0 0 0 / 0.08));
+
+  img {
+    width: 100%;
+    height: 250px;
+    object-fit: cover;
+    margin-bottom: 15px;
+  }
+  .infos {
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+    height: 100%;
+
+    .details {
+      display: flex;
+      flex-direction: row;
+      width: 100%;
+      justify-content: space-between;
+      span { 
+        color: #8e8e8e;
+        font-size: .9rem;
+      }
+    }
+    h2 {
+      font-size: 1.3rem;
+      color: #101010;
+    }
+    .price {
+      font-size: 1.3rem;
+      font-weight: 500;
+      margin-top: auto;
+      span {
+        font-size: 1.3rem;
+        margin-left: 20px;
+        text-decoration: line-through;
+        color: #5e5e5e;
+      }
+    }
+  }
 `
