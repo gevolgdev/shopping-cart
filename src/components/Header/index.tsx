bf6a322ceda13460d607902cb1ddd4349a91b814
Pinelli <ricardosilvapinelli@gmail.com>
2023-04-24 08:22:10 -0300

    Header: Navigate

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,19 @@
 import { HeaderContainer } from "./style";
 import { HiShoppingCart } from 'react-icons/hi'
+import { useNavigate } from "react-router-dom";
 
 interface HeaderProps {
   title: string;
-}
+};
 
 const Header: React.FC<HeaderProps> = (props) => {
+
+  const navigate = useNavigate();
+
   return (
     <HeaderContainer>
       <h1>{props.title}</h1>
-      <button>
+      <button onClick={() => navigate('/cart')}>
         <HiShoppingCart/>
       </button>
     </HeaderContainer>
