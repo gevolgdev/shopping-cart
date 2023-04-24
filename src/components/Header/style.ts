84c6f5d5ecfc43f97f47a3ebae30e0ec388b819f
Pinelli <ricardosilvapinelli@gmail.com>
2023-04-24 08:22:33 -0300

    Header: Cart Button

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -5,4 +5,16 @@ export const HeaderContainer = styled.header`
   flex-direction: row;
   justify-content: space-between;
   padding: 50px 0;
+
+  h1 {
+    font-size: 2.5rem;
+  }
+
+  button {
+    background: transparent;
+    border: none;
+    outline: none;
+    font-size: 2rem;
+    cursor: pointer;
+  }
 `;
