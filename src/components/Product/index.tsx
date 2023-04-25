20b410d98a6b884f3319f93a704900b1dca332d3
Pinelli <ricardosilvapinelli@gmail.com>
2023-04-25 11:56:29 -0300

    Product: Component & Design

---
+++ src/components/Product/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { ProductContainer, Button } from './style';
+import { useDispatch } from 'react-redux';
+import { addProductsCartReducer } from '../../redux/reducers/addProductReducer';
+import { DataProps } from '../../types/types';
+
+const Product: React.FC<DataProps> = (props) => {
+
+  const dispatch = useDispatch();
+
+  const addProduct = (infos: DataProps): void => {
+    dispatch(addProductsCartReducer(infos));
+  };
+
+  return (
+    <ProductContainer>
+      <img src={props.image}/>
+      <div className='infos'>
+        <div className="details">
+          <span>{props.category}</span>
+          <span>⭐ {props.rating.rate}</span>
+        </div>
+        <h2>{props.title}</h2>
+        <p>{props.description}</p>
+        <p className='price'>
+          R${props.price}
+          <span>R$ {Math.ceil(props.price + 50)}</span>
+        </p>
+      </div>
+      <Button onClick={() => addProduct(props)}>Add to cart</Button>
+    </ProductContainer>
+  )
+}
+
+export default Product
