40fff24fc297384dd55a8d23fa38dcbef4bd838a
Pinelli <ricardosilvapinelli@gmail.com>
2023-04-25 18:39:23 -0300

    Cart: Component & design

--- src/routes/Cart/index.tsx
+++ src/routes/Cart/index.tsx
@@ -1,15 +1,37 @@
+import { useState } from 'react';
 import { Header } from '../../components';
 import * as style from './style';
+import { DataProps } from '../../types/types';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../redux/reducer';
+import ProductCart from '../../components/ProductCart';
 
 const Cart = () => {
+
+  const productsCart: DataProps[] = useSelector((state: RootState) => state.addProductCart.slice(1));
+  const prices: number[] = productsCart.map((item) => item.price);
+
+  let totalCartPrice: number = 0;
+  for (let i = 0; i < prices.length; i++) {
+    totalCartPrice += prices[i];
+  };
+
+  console.log(totalCartPrice);
+
   return (
     <style.CartContainer>
       <Header
         title='Your cart'
         products
       />
+      <div className='content'>
+        {productsCart.map((item) => (
+          <ProductCart key={item.id} {...item}/>
+        ))}
+      </div>
+      ________________________ <br/><br/>
+      <h1>R$ {totalCartPrice}</h1>
     </style.CartContainer>
   )
 }
-
-export default Cart
+export default Cart;
--- src/routes/Cart/style.ts
+++ src/routes/Cart/style.ts
@@ -4,4 +4,10 @@ export const CartContainer = styled.section`
   width: 1128px;
   margin: 0 auto;
   padding-bottom: 100px;
+
+  .content {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
 `;
