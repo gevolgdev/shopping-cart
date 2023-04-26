5be24d90c8bd629304a376e4455fe03a073a97e1
Pinelli <ricardosilvapinelli@gmail.com>
2023-04-25 23:46:11 -0300

    ProductCart: Design & Price amount

--- src/components/ProductCart/index.tsx
+++ src/components/ProductCart/index.tsx
@@ -1,13 +1,29 @@
-import React from 'react'
-import { DataProps } from '../../types/types'
-import { ProductCartContainer } from './style'
+import { useState } from 'react';
+import { DataProps } from '../../types/types';
+import { ProductCartContainer } from './style';
 
 const ProductCart: React.FC<DataProps> = (props) => {
+
+  const [amount, setAmount] = useState<number>(1);
+
+  const priceProduct: number = props.price * amount;
+
   return (
     <ProductCartContainer>
-      <h1>{props.title}</h1>
+      <img src={props.image}/>
+      <div className='infos'>
+        <h1>{props.title}</h1>
+        <div className='qtd'>
+          <div className='amountInput'>
+            <button onClick={() => setAmount(prev => prev - 1)}>-</button>
+            <span>{amount}</span>
+            <button onClick={() => setAmount(prev => prev + 1)}>+</button>
+          </div>
+          <p>R$ {priceProduct.toFixed(2)}</p>
+        </div>
+      </div>
     </ProductCartContainer>
   )
 }
 
-export default ProductCart
+export default ProductCart;
--- src/components/ProductCart/style.ts
+++ src/components/ProductCart/style.ts
@@ -2,11 +2,50 @@ import styled from "styled-components";
 
 
 export const ProductCartContainer = styled.div`
+  width: 600px;
   display: flex;
   flex-direction: row;
-  border: solid 1px #101010;
+  background: white;
+  padding: 30px;
+  border-radius: 16px;
+  gap: 40px;
+
+  img {
+    width: 150px;
+  }
   
-  h1 {
-    font-size: 1.2rem;
+  .infos {
+    display: flex;
+    flex-direction: column;
+
+    h1 {
+      font-size: 1.7rem;
+      margin-bottom: 20px;
+    }
+    .qtd {
+      display: flex;
+      flex-direction: row;
+      margin-top: auto;
+
+      p {
+        margin-left: 20px;
+      }
+
+      .amountInput {
+        display: flex;
+        flex-direction: row;
+        gap: 5px;
+
+        button {
+          width: 20px;
+          height: 20px;
+          border-radius: 50px;
+          border: none;
+        }
+        span {
+          margin: 0 5px;
+        }
+      }
+    }
   }
 `;
