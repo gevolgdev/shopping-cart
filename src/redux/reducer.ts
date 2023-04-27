78b71747b21c31c2158eeaf65575b39618588186
Pinelli <ricardosilvapinelli@gmail.com>
2023-04-26 23:23:06 -0300

    Reducer: totalPriceCartReducer

--- src/redux/reducer.ts
+++ src/redux/reducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import addProductReducer from './reducers/addProductReducer';
+import totalPriceCartReducer from './reducers/totalPriceCartReducer';
 
 export const rootReducer = combineReducers({
   addProductCart: addProductReducer,
+  totalPriceCart: totalPriceCartReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>
