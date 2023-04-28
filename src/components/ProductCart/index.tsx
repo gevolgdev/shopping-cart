afea28ad4ed61dcf1ffb5f6cf4d2d6a66a1952c1
Pinelli <ricardosilvapinelli@gmail.com>
2023-04-27 21:05:14 -0300

    ProductCart: Trash button

--- src/components/ProductCart/index.tsx
+++ src/components/ProductCart/index.tsx
@@ -1,29 +1,44 @@
 import { useState } from 'react';
+import { TbTrashFilled } from 'react-icons/tb';
 import { DataProps } from '../../types/types';
 import { ProductCartContainer } from './style';
 import { useDispatch } from 'react-redux';
+import { Dispatch } from 'redux';
 import { totalPriceCartReducer } from '../../redux/reducers/totalPriceCartReducer';
+import { removeProductsCart } from '../../redux/reducers/addProductReducer';
 
-const ProductCart: React.FC<DataProps> = (props) => {
+interface Props {
+  index: number;
+  product: DataProps;
+}
+
+const ProductCart: React.FC<Props> = ({product, index}) => {
 
   const [amount, setAmount] = useState<number>(1);
-  const priceProduct: number = props.price * amount;
+  const priceProduct: number = product.price * amount;
 
   const dispatch = useDispatch();
 
   const amountPrice = () => {
-    dispatch(totalPriceCartReducer(props.price));
+    dispatch(totalPriceCartReducer(product.price));
     setAmount(prev => prev + 1);
   };
 
+  const removeProductCart = () => {
+    dispatch(removeProductsCart(index + 1));
+  }
+
   return (
     <ProductCartContainer>
-      <img src={props.image}/>
+      <img src={product.image}/>
       <div className='infos'>
-        <span>{props.category}</span>
-        <h1>{props.title}</h1>
+        <span>{index} - {product.category}</span>
+        <h1>{product.title}</h1>
         <div className='qtd'>
           <div className='amountInput'>
+            <button className='trash' onClick={removeProductCart}>
+              <TbTrashFilled/>
+            </button>
             <button disabled={amount <= 1 ? true : false} onClick={() => setAmount(prev => prev - 1)}>-</button>
             <span>{amount}</span>
             <button onClick={amountPrice}>+</button>
