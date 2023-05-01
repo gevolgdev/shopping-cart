eb53e42bd008ab30572c4f5a70099f23c2580e89
Pinelli <ricardosilvapinelli@gmail.com>
2023-05-01 16:51:28 -0300

    Cart: Responsive

--- src/routes/Cart/style.ts
+++ src/routes/Cart/style.ts
@@ -5,6 +5,11 @@ export const CartContainer = styled.section`
   margin: 0 auto;
   padding-bottom: 100px;
 
+  @media (max-width: 600px) {
+    width: 100%;
+    padding: 0 16px 100px;
+  }
+
   .content {
     display: flex;
     flex-direction: row;
